be6e8b8b7b8b9517a54ff418200de2a6d7508154
zhiqiang.wang <xv12@qq.com>
2024-09-24 22:25:27 +0800

    perf: 移除没必要的类型转换

--- app/monitor/okx.go
+++ app/monitor/okx.go
@@ -2,7 +2,6 @@ package monitor
 
 import (
 	"errors"
-	"github.com/shopspring/decimal"
 	"github.com/spf13/cast"
 	"github.com/tidwall/gjson"
 	"github.com/v03413/bepusdt/app/config"
@@ -14,14 +13,14 @@ import (
 	"time"
 )
 
-// OkxUsdtRateStart Okx USDT 汇率监控，避免频繁请求
+// OkxUsdtRateStart Okx USDT_CNY 汇率监控
 func OkxUsdtRateStart() {
 	for {
 		var rawRate, err = getOkxUsdtCnySellPrice()
 		if err != nil {
 			log.Error("Okx USDT_CNY 汇率获取失败", err)
 		} else {
-			rate.SetOkxUsdtCnyRawRate(config.GetUsdtRate(), rawRate.InexactFloat64())
+			rate.SetOkxUsdtCnyRawRate(config.GetUsdtRate(), rawRate)
 		}
 
 		log.Info("当前 USDT_CNY 计算汇率：", rate.GetUsdtCalcRate(config.DefaultUsdtCnyRate))
@@ -29,14 +28,14 @@ func OkxUsdtRateStart() {
 	}
 }
 
-// OkxTrxUsdtRateStart  Okx TRX 汇率监控
+// OkxTrxUsdtRateStart  Okx TRX_USDT 汇率监控
 func OkxTrxUsdtRateStart() {
 	for {
 		var rawRate, err = getOkxTrxUsdtRate()
 		if err != nil {
 			log.Error("Okx TRX_USDT 汇率获取失败", err)
 		} else {
-			rate.SetOkxTrxUsdtRawRate(config.GetTrxRate(), rawRate.InexactFloat64())
+			rate.SetOkxTrxUsdtRawRate(config.GetTrxRate(), rawRate)
 		}
 
 		log.Info("当前 TRX_CNY 计算汇率：", rate.GetTrxCnyCalcRate(config.DefaultTrxCnyRate))
@@ -45,8 +44,7 @@ func OkxTrxUsdtRateStart() {
 }
 
 // GetOkxTrxUsdtRate 获取 Okx TRX 汇率 https://www.okx.com/zh-hans/trade-spot/trx-usdt
-func getOkxTrxUsdtRate() (decimal.Decimal, error) {
-	var zero = decimal.Zero
+func getOkxTrxUsdtRate() (float64, error) {
 	var url = "https://www.okx.com/priapi/v5/market/candles?instId=TRX-USDT&before=1727143156000&bar=1s&limit=1&t=" + cast.ToString(time.Now().UnixNano())
 	var client = http.Client{Timeout: time.Second * 5}
 	var req, _ = http.NewRequest("GET", url, nil)
@@ -55,19 +53,19 @@ func getOkxTrxUsdtRate() (decimal.Decimal, error) {
 	resp, err := client.Do(req)
 	if err != nil {
 
-		return zero, errors.New("okx resp error:" + err.Error())
+		return 0, errors.New("okx resp error:" + err.Error())
 	}
 
 	defer resp.Body.Close()
 	if resp.StatusCode != 200 {
 
-		return zero, errors.New("okx resp status code:" + strconv.Itoa(resp.StatusCode))
+		return 0, errors.New("okx resp status code:" + strconv.Itoa(resp.StatusCode))
 	}
 
 	all, err := io.ReadAll(resp.Body)
 	if err != nil {
 
-		return zero, errors.New("okx resp read body error:" + err.Error())
+		return 0, errors.New("okx resp read body error:" + err.Error())
 	}
 
 	/**
@@ -95,16 +93,15 @@ func getOkxTrxUsdtRate() (decimal.Decimal, error) {
 		var data = result.Get("data").Array()
 		if len(data) > 0 {
 
-			return decimal.NewFromFloat(data[0].Get("1").Float()), nil
+			return data[0].Get("1").Float(), nil
 		}
 	}
 
-	return zero, errors.New("okx resp json data not found")
+	return 0, errors.New("okx resp json data not found")
 }
 
 // getOkxUsdtCnySellPrice  Okx  C2C快捷交易 USDT出售 实时汇率
-func getOkxUsdtCnySellPrice() (decimal.Decimal, error) {
-	var _zero = decimal.NewFromInt(0)
+func getOkxUsdtCnySellPrice() (float64, error) {
 	var t = strconv.Itoa(int(time.Now().Unix()))
 	var okxApi = "https://www.okx.com/v4/c2c/express/price?crypto=USDT&fiat=CNY&side=sell&t=" + t
 	client := http.Client{Timeout: time.Second * 5}
@@ -113,35 +110,35 @@ func getOkxUsdtCnySellPrice() (decimal.Decimal, error) {
 	resp, err := client.Do(req)
 	if err != nil {
 
-		return _zero, errors.New("okx resp error:" + err.Error())
+		return 0, errors.New("okx resp error:" + err.Error())
 	}
 
 	defer resp.Body.Close()
 	if resp.StatusCode != 200 {
 
-		return _zero, errors.New("okx resp status code:" + strconv.Itoa(resp.StatusCode))
+		return 0, errors.New("okx resp status code:" + strconv.Itoa(resp.StatusCode))
 	}
 
 	all, err := io.ReadAll(resp.Body)
 	if err != nil {
 
-		return _zero, errors.New("okx resp read error:" + err.Error())
+		return 0, errors.New("okx resp read error:" + err.Error())
 	}
 
 	result := gjson.ParseBytes(all)
 	if result.Get("error_code").Int() != 0 {
 
-		return _zero, errors.New("json parse error:" + result.Get("error_message").String())
+		return 0, errors.New("json parse error:" + result.Get("error_message").String())
 	}
 
 	if result.Get("data.price").Exists() {
 		var _ret = result.Get("data.price").Float()
 		if _ret <= 0 {
-			return _zero, errors.New("okx resp json data.price <= 0")
+			return 0, errors.New("okx resp json data.price <= 0")
 		}
 
-		return decimal.NewFromFloat(_ret), nil
+		return cast.ToFloat64(_ret), nil
 	}
 
-	return _zero, errors.New("okx resp json data.price not found")
+	return 0, errors.New("okx resp json data.price not found")
 }
