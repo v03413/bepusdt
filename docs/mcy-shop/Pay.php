53c4c0788cff4fb122dcf6d063c2750f5ef78724
zhiqiang.wang <xv12@qq.com>
2025-02-14 11:53:22 +0800

    docs: 萌次元商城系统对接教程

---
+++ docs/mcy-shop/Pay.php
@@ -0,0 +1,9 @@
+<?php
+declare (strict_types=1);
+return [
+    'alipay'     => '支付宝',
+    'wxpay'      => '微信',
+    'qqpay'      => 'QQ钱包',
+    'usdt.trc20' => 'USDT.TRC20',
+    'tron.trx'   => 'TRON.TRX',
+];
