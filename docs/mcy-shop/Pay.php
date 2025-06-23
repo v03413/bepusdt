60a53b0e3a3b37d28ff8e3074dadb63df87d85a8
v03413 <admin@qzone.work>
2025-06-23 17:51:01 +0800

    perf: 梦次元易支付插件适配

--- docs/mcy-shop/Pay.php
+++ docs/mcy-shop/Pay.php
@@ -1,9 +1,14 @@
 <?php
 declare (strict_types=1);
+
 return [
-    'alipay'     => '支付宝',
-    'wxpay'      => '微信',
-    'qqpay'      => 'QQ钱包',
-    'usdt.trc20' => 'USDT.TRC20',
-    'tron.trx'   => 'TRON.TRX',
+    'alipay'       => '支付宝',
+    'wxpay'        => '微信',
+    'qqpay'        => 'QQ钱包',
+    'tron.trx'     => 'TRON・TRX',
+    'usdt.trc20'   => 'USDT・TRC20',
+    'usdt.bep20'   => 'USDT・BEP20',
+    'usdt.erc20'   => 'USDT・ERC20',
+    'usdt.polygon' => 'USDT・Polygon',
+    'usdt.xlayer'  => 'USDT・X-Layer',
 ];
