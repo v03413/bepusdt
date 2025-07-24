b7d3c04e4e2136551bd789fb3208aa50d9599984
v03413 <admin@qzone.work>
2025-07-24 11:07:59 +0800

    perf: polygon 默认RPC节点调整

--- app/conf/const.go
+++ app/conf/const.go
@@ -11,15 +11,15 @@ const (
 	defaultUsdtAtomicity    = 0.01 // 原子精度
 	defaultTrxAtomicity     = 0.01
 
-	defaultTronGrpcNode        = "18.141.79.38:50051"                 // 默认GRPC节点
-	defaultBscRpcEndpoint      = "https://bsc-dataseed.bnbchain.org/" // 默认BSC RPC节点
-	defaultSolanaRpcEndpoint   = "https://solana-rpc.publicnode.com/" // 默认Solana RPC节点 官方是 https://api.mainnet-beta.solana.com/ 但存在速率限制
-	defaultXlayerRpcEndpoint   = "https://xlayerrpc.okx.com/"         // 默认Xlayer RPC节点
-	defaultPolygonRpcEndpoint  = "https://polygon-rpc.com/"           // 默认Polygon RPC节点
-	defaultEthereumRpcEndpoint = "https://ethereum.publicnode.com/"   // 默认Ethereum RPC节点
-	defaultAptosRpcEndpoint    = "https://aptos-rest.publicnode.com/" // 默认Aptos RPC节点
-	defaultOutputLog           = "/var/log/bepusdt.log"               // 默认日志输出文件
-	defaultSqlitePath          = "/var/lib/bepusdt/sqlite.db"         // 默认数据库文件
+	defaultTronGrpcNode        = "18.141.79.38:50051"                      // 默认GRPC节点
+	defaultBscRpcEndpoint      = "https://bsc-dataseed.bnbchain.org/"      // 默认BSC RPC节点
+	defaultSolanaRpcEndpoint   = "https://solana-rpc.publicnode.com/"      // 默认Solana RPC节点 官方是 https://api.mainnet-beta.solana.com/ 但存在速率限制
+	defaultXlayerRpcEndpoint   = "https://xlayerrpc.okx.com/"              // 默认Xlayer RPC节点
+	defaultPolygonRpcEndpoint  = "https://polygon-bor-rpc.publicnode.com/" // 默认Polygon RPC节点 官方 https://polygon-rpc.com 存在速率限制
+	defaultEthereumRpcEndpoint = "https://ethereum.publicnode.com/"        // 默认Ethereum RPC节点
+	defaultAptosRpcEndpoint    = "https://aptos-rest.publicnode.com/"      // 默认Aptos RPC节点
+	defaultOutputLog           = "/var/log/bepusdt.log"                    // 默认日志输出文件
+	defaultSqlitePath          = "/var/lib/bepusdt/sqlite.db"              // 默认数据库文件
 )
 
 const (
